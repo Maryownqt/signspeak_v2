db6b110ff334f4a519fc333de860188f0addbfe0
Maryownqt <janmarion16@gmail.com>
2025-05-09 18:38:54 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const streamUrl = '/video_feed';  // will be proxied via Netlify/_redirects
+
   return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '2rem'
+      }}
+    >
       <h1>Live SignSpeak Stream</h1>
       <iframe
-        src="/video_feed"
+        src={streamUrl}
         width="640"
         height="480"
-        style={{ border: '2px solid #333' }}
-        title="Live SignSpeak"
+        style={{ border: '2px solid #333', marginTop: '1rem' }}
+        title="Live SignSpeak Stream"
       />
     </div>
-  );
-}
-
   );
 }
 
