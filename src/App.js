3157592a5884c524ff81ebba8944f8e00e90cab5
Maryownqt <janmarion16@gmail.com>
2025-05-09 18:35:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{
-      display: 'flex', flexDirection: 'column',
-      alignItems: 'center', padding: '2rem'
-    }}>
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
       <h1>Live SignSpeak Stream</h1>
-      <div style={{
-        width: '640px', height: '480px',
-        border: '2px solid #333', marginTop: '1rem'
-      }}>
-        <img
-          src="/video_feed"
-          alt="Live backend stream"
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          crossOrigin="anonymous"
-        />
-      </div>
+      <iframe
+        src="/video_feed"
+        width="640"
+        height="480"
+        style={{ border: '2px solid #333' }}
+        title="Live SignSpeak"
+      />
     </div>
+  );
+}
+
   );
 }
 
